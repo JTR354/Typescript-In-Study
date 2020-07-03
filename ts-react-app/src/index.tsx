daa3f2d289cb0fec48152b3d1fe9ec823b5c9189
jtr <jtr354@foxmail.com>
2020-07-03 15:59:45 +0800

    redux

--- ts-react-app/src/index.tsx
+++ ts-react-app/src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import Router from './routers'
 
 import store from './redux/store'
-
+import 'antd/dist/antd.css'
 // import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
