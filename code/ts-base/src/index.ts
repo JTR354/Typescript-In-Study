0a22bcc70effc45dee8ae9de91042dff0fbbab10
jtr354 <jtr354@foxmail.com>
2020-07-13 09:26:23 +0800

    function class

--- code/ts-base/src/index.ts
+++ code/ts-base/src/index.ts
@@ -1,6 +1,8 @@
 import './part1.base/05.datatype'
 import './part1.base/06.enum'
 import './part1.base/07-08.interface'
+import './part1.base/09.function'
+import './part1.base/10-11.class'
 
 let hello: string = 'Hello TypeScript'
 document.querySelectorAll('#app')[0].innerHTML = hello
