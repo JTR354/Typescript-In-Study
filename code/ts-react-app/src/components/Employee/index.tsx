af311dc6b4e9b86157ad8a9a72c7cb720afe38ee
jtr354 <jtr354@foxmail.com>
2020-07-15 09:27:25 +0800

    finish component demo

--- code/ts-react-app/src/components/Employee/index.tsx
+++ code/ts-react-app/src/components/Employee/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Hello from '../demo/Hello'
-// import HelloClass from '../demo/HelloClass'
+import HelloClass from '../demo/HelloClass'
 import HelloHoc from '../demo/HelloHoc'
+import HelloHooks from '../demo/HelloHooks'
 
 const Employee = () => {
+  let [loading, setLoading] = useState(true)
+  useEffect(
+    () => {
+      setTimeout(() => {
+        setLoading(false)
+      }, 1000)
+      // console.log(1);
+    },
+    [loading]
+  )
   return (
     <>
       <h1>hello employee</h1>
       <Hello name='123'></Hello>
-      {/* <HelloClass name='cls'></HelloClass> */}
-      <HelloHoc name='hoc' loading={true}/>
+      <HelloClass name='cls'></HelloClass>
+      <HelloHoc name='hoc' loading={loading}/>
+      <HelloHooks name='hooks'></HelloHooks>
     </>
   )
 }
